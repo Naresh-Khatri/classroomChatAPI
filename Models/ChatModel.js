10349701502c4c17a0484cfede8368a7b3d425b6
Naresh <naresh.khatri2345@gmail.com>
2021-12-28 17:39:18 +0530

    Add db with few enpoints

--- Models/ChatModel.js
+++ Models/ChatModel.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose')
 
 const chatsDataSchema = mongoose.Schema({
-    username: String,
+    user: {
+        type: String,
+        ref: 'User',
+    },
+    name: String,
     text: String,
+    photoURL: String,
+    reactions: [{
+        reaction: String,
+        user: {
+            type: String,
+            ref: 'User',
+        },
+        time: Date,
+    }],
     timestamp: {
         type: Date,
         default: Date
     }
 })
 
-module.exports = mongoose.model('Chat',chatsDataSchema)
+module.exports = mongoose.model('Chat', chatsDataSchema)
