8d6a3b539db04079840df147da4df6882b91ed53
Naresh <naresh.khatri2345@gmail.com>
2022-01-16 23:55:22 +0530

    Add DELETE /uploads/material/:id endpoint

--- routes/upload.js
+++ routes/upload.js
@@ -1,77 +1,92 @@
-import fs from 'fs'
+import fs from "fs";
 
-import express from 'express'
-import multer from 'multer'
+import express from "express";
+import multer from "multer";
 // import pdf from 'pdf-thumbnail'
 
-import StudyMaterial from '../Models/StudyMaterial.js'
+import StudyMaterial from "../Models/StudyMaterial.js";
 
-
-const router = express.Router()
+const router = express.Router();
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
+  destination: (req, file, cb) => {
+    cb(null, "uploads/materials");
+  },
+  filename: (req, file, cb) => {
+    console.log(req.body);
+    cb(null, `${req.body.uid}-${Date.now()}.${req.body.fileName}`);
+  },
+});
 
-        cb(null, 'uploads/materials')
-    },
-    filename: (req, file, cb) => {
-        console.log(req.body)
-        cb(null, `${req.body.uid}-${Date.now()}.${req.body.fileName}`)
-    }
-})
-
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 //upload attachment
-router.post('/pdf', upload.single('file'), async (req, res) => {
-    console.log(req.file)
-    console.log(req.body)
-    //create new study material object
-    const studyMaterialInfo = {}
+router.post("/pdf", upload.single("file"), async (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+  //create new study material object
+  const studyMaterialInfo = {};
+  try {
     studyMaterialInfo.title = req.body.title;
     studyMaterialInfo.description = req.body.description;
     studyMaterialInfo.subject = req.body.subject;
-    studyMaterialInfo.classroomID = req.body.classroomID
-    studyMaterialInfo.createdUser = req.body.uid
+    studyMaterialInfo.classroomID = req.body.classroomID;
+    studyMaterialInfo.createdUser = req.body.uid;
     // studyMaterialInfo.classroomID = req.body.classroomID
 
     // studyMaterialInfo.previewPath = await createPdfPreview(req.file.filename)
-    studyMaterialInfo.fileName = req.file.filename
-    studyMaterialInfo.fileType = req.file.mimetype.split('/')[1]
-    studyMaterialInfo.fileSize = req.file.size
-    studyMaterialInfo.filePath = req.file.path
-    const studyMaterial = new StudyMaterial(
-        studyMaterialInfo
-    )
-    studyMaterial.save()
-    res.send('pdf uploaded')
+    studyMaterialInfo.fileName = req.file.filename;
+    studyMaterialInfo.fileType = req.file.mimetype.split("/")[1];
+    studyMaterialInfo.fileSize = req.file.size;
+    studyMaterialInfo.filePath = req.file.path;
+    const studyMaterial = new StudyMaterial(studyMaterialInfo);
+    studyMaterial.save();
+    res.send("pdf uploaded");
+  } catch (err) {
+    console.log('error when uploading pdf', err);
+    res.status(400).send({ msg: "check your inputs" });
+  }
+});
 
+router.get("/materials/:classroomID", (req, res) => {
+  StudyMaterial.find({ classroomID: req.params.classroomID })
+    .sort({ timestamp: -1 })
+    .exec((err, studyMaterials) => {
+      if (err) {
+        console.log("error in finding study materials", err);
+        res.status(404).send({ message: "error in finding study materials" });
+      }
+      res.send(studyMaterials);
+    });
+});
+router.get("/material/:id", (req, res) => {
+  console.log("getting material", req.params.id);
+  StudyMaterial.findById(req.params.id, (err, studyMaterial) => {
+    if (err) {
+      console("error while getting single study material", err);
+      res
+        .status(404)
+        .send({ message: "error while getting single study material" });
+    }
+    res.download("uploads/materials/" + studyMaterial.fileName, "okayokay.pdf");
+  });
+});
 
-})
+router.delete("/material/:id", (req, res) => {
+  console.log("deleting material", req.params);
+  // res.send('deleting material');
+  StudyMaterial.findByIdAndDelete(req.params.id, (err, studyMaterial) => {
+    if (err) {
+      console.log("couldnt delete study material", err);
+      res.status(404).send({ message: "couldnt delete study material" });
+    }
+    res.status(200).send({ message: "study material deleted" });
+  });
+});
 
-router.get('/materials/:classroomID', (req, res) => {
-    StudyMaterial.find({ classroomID: req.params.classroomID }, (err, studyMaterials) => {
-        if (err) {
-            console.log('error in finding study materials', err)
-            res.status(404).send({ message: 'error in finding study materials' })
-        }
-        res.send(studyMaterials)
-    })
-})
-router.get('/material/:id', (req, res) => {
-    console.log('getting material', req.params.id)
-    StudyMaterial.findById(req.params.id, (err, studyMaterial) => {
-        if (err) {
-            console('error while getting single study material', err)
-            res.status(404).send({ message: 'error while getting single study material' })
-        }
-        res.download('uploads/materials/' + studyMaterial.fileName, 'sdf')
-    })
-})
-
-router.get('/', (req, res) => {
-    res.send('hello')
-})
+router.get("/", (req, res) => {
+  res.send("hello");
+});
 
 // const createPdfPreview = (filename) => {
 //     return new Promise((resolve, reject) => {
@@ -89,6 +104,4 @@ router.get('/', (req, res) => {
 //     })
 // }
 
-export default router
-
-
+export default router;
--- test.js
+++ test.js
@@ -1,13 +1,23 @@
-import pdf from 'pdf-thumbnail'
 import fs from 'fs'
+import pdf2img from 'pdf-img-convert'
 
-const pdfBuffer = fs.readFileSync('./uploads/materials/ipZVQgDR5Wdixl5EfR48I6rPtkG3-1642055742380.CSE-R19_2nd-Year-Course-Structure-Syllabus.pdf');
+const url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf'
 
-pdf(pdfBuffer, /*Buffer or stream of the pdf*/)
-    .then(data /*Stream of the image*/ => {
-        data.pipe(fs.createWriteStream('./uploads/materials/example.jpg'));
-    })
-    .catch(err => console.log(err))
+const img = await pdf2img.convert(url)
+console.log(img)
+
+
+
+
+// import pdf from 'pdf-thumbnail'
+
+// const pdfBuffer = fs.readFileSync('./uploads/materials/ipZVQgDR5Wdixl5EfR48I6rPtkG3-1642055742380.CSE-R19_2nd-Year-Course-Structure-Syllabus.pdf');
+
+// pdf(pdfBuffer, /*Buffer or stream of the pdf*/)
+//     .then(data /*Stream of the image*/ => {
+//         data.pipe(fs.createWriteStream('./uploads/materials/example.jpg'));
+//     })
+//     .catch(err => console.log(err))
 
 // pdf(fs.createReadStream(`./uploads/materials/ipZVQgDR5Wdixl5EfR48I6rPtkG3-1642054020086.CSE-R19_2nd-Year-Course-Structure-Syllabus.pdf`))
 //     .then(data /*is a stream*/ => {
